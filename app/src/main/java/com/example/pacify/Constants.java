d7f2a8733bcfa6a840a7e1c6a38efa6f60adf991
ABDULRAHMANHAMMOUDA <abdulrahmanhammouda@outlook.com>
2020-04-09 22:56:39 +0200

    Fixed issue with notification

--- app/src/main/java/com/example/pacify/Constants.java
+++ app/src/main/java/com/example/pacify/Constants.java
@@ -19,9 +19,8 @@ public class Constants {
     }
 
     public interface PLAYLIST_ID {
-        public static String POP = "http://192.168.1.6:5000/Playlist/5e8e0396a1a3730480cc0a66";
-        public static String ELECTRONIC= "http://192.168.1.6:5000/Playlist/5e8e0396a1a3730480cc0a68";
+        public static String POP = "http://192.168.43.127:5000/allSongs";
+        public static String ELECTRONIC = "http://192.168.43.127:5000/Playlist/5e8e0396a1a3730480cc0a68";
     }
-
-    public static String SIGNUP_URL = "http://192.168.1.6:5000/signup";
+    public static String SIGNUP_URL = "http://192.168.43.127:5000/signup";
 }
