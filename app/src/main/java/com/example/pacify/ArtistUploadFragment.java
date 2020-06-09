ce56715f6bd500afd0a9cfb0f39188751c0c84c0
yara wael mohamed <yarawaelmohamed@gmail.com>
2020-06-09 17:03:59 +0200

    Add profile pic, Download and Share song

--- app/src/main/java/com/example/pacify/ArtistUploadFragment.java
+++ app/src/main/java/com/example/pacify/ArtistUploadFragment.java
@@ -1,18 +1,26 @@
 package com.example.pacify;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ArtistUploadFragment extends Fragment {
 
     Button logoutBtn;
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView userPhoto;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container
@@ -27,7 +35,28 @@ public class ArtistUploadFragment extends Fragment {
                 ((ArtistActivity)requireActivity()).Logout();
             }
         });
+        userPhoto = (ImageView) view.findViewById (R.id.userPhoto);
+        userPhoto.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.userPhoto:
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                        break;
+                };
+            }
+        });
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data ){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data !=null){
+            Uri selectedImage = data.getData ();
+            userPhoto.setImageURI(selectedImage);
+        };
+    };
 }
